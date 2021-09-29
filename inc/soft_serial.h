fa108a3b330c4da30ba779ef09e0c2191367d33a
qiyongzhong0 <917768104@qq.com>
2021-09-29 15:51:33 +0800

    fix soft_serial.h

--- inc/soft_serial.h
+++ inc/soft_serial.h
@@ -12,24 +12,32 @@
 #include <soft_serial_stm32f103rc.h>
 #include <soft_serial_stm32f103ze.h>
 
-#ifndef SS_TIM_CLK_FREQ
+#ifdef SOFT_SERIAL_TIM_CLK_FREQ
+#define SS_TIM_CLK_FREQ     SOFT_SERIAL_TIM_CLK_FREQ 
+#else
 #define SS_TIM_CLK_FREQ     8000000
 #endif
 
-#ifndef SS_TIM_PCLK1_MUL
-#define SS_TIM_PCLK1_MUL    2 //APB1 timer clock multiplying power
+//APB1 timer clock frequency multiplier
+#ifdef SOFT_SERIAL_TIM_PCLK1_MUL
+#define SS_TIM_PCLK1_MUL    SOFT_SERIAL_TIM_PCLK1_MUL 
+#else
+#define SS_TIM_PCLK1_MUL    1 
 #endif
 
-#ifndef SS_TIM_PCLK2_MUL
-#define SS_TIM_PCLK2_MUL    1 //APB1 timer clock multiplying power
+//APB2 timer clock frequency multiplier
+#ifdef SOFT_SERIAL_TIM_PCLK2_MUL
+#define SS_TIM_PCLK2_MUL    SOFT_SERIAL_TIM_PCLK2_MUL 
+#else
+#define SS_TIM_PCLK2_MUL    1
 #endif
 
-#ifndef SS_CHK_RX_BEGIN_BIT
+#ifdef SOFT_SERIAL_CHK_RX_BEGIN_BIT
 #define SS_CHK_RX_BEGIN_BIT //using recv begin bit check, not recommended when baudrate is high 
 #endif
 
-#ifndef SS_CHK_RX_PARITY
-//#define SS_CHK_RX_PARITY  //using recv parity bit check, not recommended
+#ifdef SOFT_SERIAL_CHK_RX_PARITY
+#define SS_CHK_RX_PARITY  //using recv parity bit check, not recommended
 #endif
 
 int ss_scoms_init(void);//initialize and register soft serial, it is auto initialized
